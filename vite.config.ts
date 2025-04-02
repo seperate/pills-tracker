922104e95ba8f96b351afb56db68dc2f24669949
seperate <beneyupcingel@gmail.com>
2025-04-02 16:47:20 +0300

    Netlify support added

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pills-tracker/', // Replace with your repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
